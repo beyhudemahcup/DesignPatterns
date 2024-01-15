c22e916d8fa742280bdf19642b3aa99c9c2f9705
beyhudemahcup <beyhudemahcup@gmail.com>
2024-01-15 19:53:22 +0100

    decorator design pattern is added

---
+++ DecoratorDesignPattern/ApplePhone.cs
@@ -0,0 +1,24 @@
+﻿namespace DecoratorDesignPattern
+{
+    public class ApplePhone : IPhone
+    {
+        //defining base class
+        public int CameraCount()
+        {
+            //IPhones has one camera at least 
+            return 1;
+        }
+
+        public string GetName()
+        {
+            //name starts with IPhone all the time
+            return "IPhone";
+        }
+
+        public double GetPrice()
+        {
+            //price is at least 500 eur 
+            return 499.9;
+        }
+    }
+}
---
+++ DecoratorDesignPattern/ApplePhone14.cs
@@ -0,0 +1,24 @@
+﻿namespace DecoratorDesignPattern
+{
+    public class ApplePhone14 : PhoneDecorator
+    {
+        public ApplePhone14(ApplePhone iphone) : base(iphone)
+        {
+        }
+
+        public override string GetName()
+        {
+            return _iphone.GetName() + " " + "14";
+        }
+        
+        public override int CameraCount()
+        {
+            return _iphone.CameraCount() + 1;//iphone 14 has 2 cameras
+        }
+
+        public override double GetPrice()
+        {
+            return _iphone.GetPrice();
+        }
+    }
+}
